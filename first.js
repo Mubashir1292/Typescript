8041acb2032457ec6d4e5f6c7f9cf0d32a4378db
Mubsahir1292 <mubashirliaqat72@gmail.com>
2024-11-23 13:38:01 +0500

    done the basics

---
+++ first.js
@@ -0,0 +1,7 @@
+var users = [
+    { name: 'Mubashir1292', email: 'mubashirliaqat72@gmail.com' },
+    { name: 'Mubashir1292', email: 'mubashirliaqat72@gmail.com' },
+    { name: 'Mubashir1292', email: 'mubashirliaqat72@gmail.com' },
+];
+users.map(function (item, index) { return (console.log(item)); });
+console.log("Typescript Started");
---
+++ first.ts
@@ -0,0 +1,10 @@
+let users=[
+    {name:'Mubashir1292',email:'mubashirliaqat72@gmail.com'},
+    {name:'Mubashir1292',email:'mubashirliaqat72@gmail.com'},
+    {name:'Mubashir1292',email:'mubashirliaqat72@gmail.com'},
+]
+
+users.map((item,index)=>(
+    console.log(item)
+));
+console.log("Typescript Started");
