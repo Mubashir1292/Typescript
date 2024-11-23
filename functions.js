f802c9872199c462f45a0ea1c9eaed0460142423
Mubsahir1292 <mubashirliaqat72@gmail.com>
2024-11-23 20:06:08 +0500

    functions

---
+++ functions.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    return num + 3;
+}
+console.log(addTwo(4));
+function signup(name, email, password) {
+    console.log(name, email, password);
+}
+signup("2", "3", "4");
+function login(name, id) {
+    console.log(name, id);
+}
+login("Mubashir", 3);
---
+++ functions.ts
@@ -0,0 +1,14 @@
+function addTwo(num:number){
+    return num+3;
+}
+console.log(addTwo(4));
+
+function signup(name:string,email:string,password:string){
+    console.log(name,email,password);
+}
+signup("2","3","4");
+function login(name:string,id:number){
+    return name+id;
+}
+login("Mubashir",3);
+export {}
