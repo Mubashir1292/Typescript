52da8b1bfff4ca976caaa29e0c12ef79f1bb7fed
Mubsahir1292 <mubashirliaqat72@gmail.com>
2024-12-03 13:28:04 +0500

    assued

--- Obects/MyObjects.ts
+++ Obects/MyObjects.ts
@@ -4,5 +4,16 @@ function printingUser({name,email}:{name:string,email:string}){
     console.log(name,email);
 }printingUser({name:"asdljdas",email:"kjhjkhkj"});
 
+//! we are working on objects. 
+//? type Aliases
+
+type user={
+    id:number,
+    name:string,
+    isActive:boolean
+}
+
+
+
 //? we are currently working onto the some working
 export{}    
